ad61abf11a19689f7d58053fbff450c51337ecb6
Rajarshi Khare <rajarshi_vaibhav@turbohire.co>
2023-08-27 16:11:13 +0530

    asdf

--- src/components/VQAImages.js
+++ src/components/VQAImages.js
@@ -6,7 +6,7 @@ import { Box, ImageList } from "@mui/material";
 const VQAImages = ({ images }) => {
   return (
     <Box display="flex" pt={2} px={8}>
-      <ImageList cols={4} sx={{ width: "100%", height: "84vh" }} gap={2}>
+      <ImageList cols={4} sx={{ width: "100%", height: "84vh" }} gap={16}>
         {images.map((image) => {
           return <VQAImage image={image} />;
         })}
