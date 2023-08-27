a3bf455fa87cefd29386bfe08d7ddd9dc6ce8c3c
Rajarshi Khare <rajarshi_vaibhav@turbohire.co>
2023-08-27 14:41:21 +0530

    Change

--- src/components/VQAImages.js
+++ src/components/VQAImages.js
@@ -6,7 +6,7 @@ import { Box, ImageList } from "@mui/material";
 const VQAImages = ({ images }) => {
   return (
     <Box display="flex" p={2} px={8}>
-      <ImageList cols={4} sx={{ width: "100%", height: "80vh" }}>
+      <ImageList cols={4} sx={{ width: "100%", height: "84vh" }} gap={2}>
         {images.map((image) => {
           return <VQAImage image={image} />;
         })}
