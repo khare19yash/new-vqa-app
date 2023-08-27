5907b1b410ae7530c88755d382878a24680ff9e7
Rajarshi Khare <rajarshi_vaibhav@turbohire.co>
2023-08-27 16:17:09 +0530

    ad

--- src/components/VQAHeader.js
+++ src/components/VQAHeader.js
@@ -4,7 +4,7 @@ import DoneIcon from "@mui/icons-material/Done";
 
 const VQAHeader = ({ datasets, onDatasetClick, searchQueury, onChangeSearch, selectedDataset = "roco" }) => {
   return (
-    <Box px={8} py={1} pb={0} display={"flex"} alignItems={"center"}>
+    <Box px={8} py={1} pb={0} display={"flex"} alignItems={"center"} position={"fixed"} top={0} width={1}>
       <Box>
         <img src={"/logo.svg"} style={{height: 48}} />
       </Box>
@@ -28,32 +28,6 @@ const VQAHeader = ({ datasets, onDatasetClick, searchQueury, onChangeSearch, sel
 };
 
 const styles = {
-  header: {
-    padding: "1rem",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  title: {
-    fontSize: "1.5rem",
-    margin: 0,
-  },
-  nav: {},
-  navList: {
-    listStyle: "none",
-    margin: 0,
-    padding: 0,
-    display: "flex",
-  },
-  navItem: {
-    marginLeft: "1rem",
-  },
-  button: {
-    border: "none",
-    backgroundColor: "transparent",
-    color: "#fff",
-    cursor: "pointer",
-  },
 };
 
 export default VQAHeader;
