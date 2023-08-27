d8d4c54477b9006aea902370ce755b36edd74780
Rajarshi Khare <rajarshi_vaibhav@turbohire.co>
2023-08-27 14:56:36 +0530

    Change

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
+import { createTheme } from '@mui/material';
 import './App.css';
 import VQAMain from './components/VQAMain';
 
+
 const theme = createTheme({
-  components: {
-    // Name of the component
-    MuiButtonBase: {
-      defaultProps: {
-        // The props to change the default for.
-        disableRipple: true, // No more ripple, on the whole application 💣!
-      },
-    },
-  },
 });
 
 
